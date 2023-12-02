ee42df2c13a2ab31281f1adc4d74c1d7d4245760
xtuna <92073982+xtuna@users.noreply.github.com>
2023-12-03 02:09:16 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+// Listening to the video element
+    let clip = document.querySelector(".vid");
 
+    /* Adding the event listeners on the video to play/pause the video. */
+    clip.addEventListener("mouseover", function (e) {
+        clip.play();
+    })
+
+    /* Applying the mouse out event to pause the video */
+    clip.addEventListener("mouseout", function (e) {
+        clip.pause();
+    })
